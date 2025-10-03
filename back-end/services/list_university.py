aee338e30efbd4552e1ee7dff5d209e1a2e243a0
TuanSu-LoneWolf <tuansu1561@gmail.com>
2025-10-03 17:36:31 +0700

    update port

--- back-end/services/list_university.py
+++ back-end/services/list_university.py
@@ -125,4 +125,6 @@ def university_detail(school_code):
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
--- front-end/src/pages/CheckCVPage/checkCVPage.jsx
+++ front-end/src/pages/CheckCVPage/checkCVPage.jsx
@@ -1,12 +1,16 @@
-
-
 export function CheckCVPage() {
   return (
     <div className="flex flex-col justify-center items-center max-w-3xl mx-auto pb-16">
-      
-      <div className="max-w-lg border border-[var(--border)] bg-[var(--card)] rounded-[var(--radius)]">
-        
+      <div className="p-16">
+        <h1 className="text-3xl text-[var(--card-foreground)] text-center mb-2">
+          Khám phá cơ hội học tập
+        </h1>
+        <p className="text-[var(--muted-foreground)] text-center mb-4 md:mb-12">
+          Tìm hiểu thông tin chi tiết về các trường đại học và nhóm ngành đào
+          tạo để đưa ra lựa chọn phù hợp nhất
+        </p>
       </div>
+      <div className="max-w-lg border border-[var(--border)] bg-[var(--card)] rounded-[var(--radius)]"></div>
     </div>
-  )
+  );
 }
